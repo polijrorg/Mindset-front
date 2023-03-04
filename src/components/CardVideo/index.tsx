12eecb08e1dc4effd77ecf76d9050c108b53b02d
AnnaK-Cardoso <annakarolinecardoso123@gmail.com>
2023-03-03 22:17:12 -0300

    Add: Card Video Component

---
+++ src/components/CardVideo/index.tsx
@@ -0,0 +1,41 @@
+import { ProgressBar } from 'react-bootstrap';
+import * as S from './styles';
+
+export type CardComponentProps = {
+    description: string;
+    title: string;
+    image?: string;
+    isWhite: boolean;
+};
+
+const CardVideo: React.FC<CardComponentProps> = ({
+    title,
+    description,
+    image,
+    isWhite
+}) => (
+    <S.Container isWhite={isWhite}>
+        <S.SubContainer>
+            {isWhite ? (
+                <S.Image src="/assets/imgOptionBlack.svg" />
+            ) : (
+                <S.Image src="/assets/imgOptionWhite.svg" />
+            )}
+
+            <S.SubContainer2>
+                <S.Title>{title}</S.Title>
+                <S.Text>{description}</S.Text>
+            </S.SubContainer2>
+            <S.ButtonImage>
+                {isWhite ? (
+                    <img src="/assets/playwhite.svg" alt="play" />
+                ) : (
+                    <img src="/assets/blackplay.svg" alt="play" />
+                )}
+            </S.ButtonImage>
+        </S.SubContainer>
+        <S.ProgressBar value={40} max={100} />
+    </S.Container>
+);
+export default CardVideo;
+/* utilizar o use Effect https://www.google.com/search?q=styled+progress+bar&source=lnms&tbm=vid&sa=X&ved=2ahUKEwiZvKW69sD9AhUhr5UCHYapCDgQ_AUoAnoECAEQBA&biw=1440&bih=671&dpr=2#fpstate=ive&vld=cid:eef93ea1,vid:3sH_Kq9e5hQ */
---
+++ src/components/CardVideo/styles.ts
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+interface SelectedProps {
+    isWhite: boolean;
+}
+export const Container = styled.div<SelectedProps>`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 49px 21px 28px 24px;
+    gap: 20px;
+    margin: 64px;
+    position: absolute;
+    width: 500px;
+    height: 277px;
+    background: ${(props) => (props.isWhite ? '#ffffff' : '#272727')};
+    box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.14), 0px 3px 3px rgba(0, 0, 0, 0.12),
+        0px 1px 8px rgba(0, 0, 0, 0.2);
+    border-radius: 16px;
+`;
+export const SubContainer = styled.div`
+    display: flex;
+    flex-direction: colum;
+    align-items: center;
+    gap: 14px;
+    padding: 0px;
+    width: fit-content;
+`;
+export const SubContainer2 = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 0px;
+    width: fit-content;
+    height: 176px;
+`;
+export const Image = styled.img`
+    right: 65.47%;
+    top: 6.25%;
+    bottom: 6.25%;
+    border: 6px solid #ffffff;
+`;
+export const Title = styled.span<SelectedProps>`
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 28px;
+    line-height: 42px;
+    color: ${(props) => (props.isWhite ? '#ffffff' : '#272727')};
+`;
+export const Text = styled.span<SelectedProps>`
+    font-family: 'Poppins';
+    font-style: italic;
+    font-weight: 300;
+    font-size: 20px;
+    line-height: 24px;
+    color: ${(props) => (props.isWhite ? '#ffffff' : '#272727')};;
+`;
+export const ButtonImage = styled.a`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+`;
+export const ProgressBar = styled.progress`
+    accent-color: #eeac0e;
+    width: 432px;
+    height: 4px;
+    cursor: pointer;
+`;
