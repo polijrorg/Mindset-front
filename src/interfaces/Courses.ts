1d3fdfc7d1c226bf0212b6d7add19aeecfe16cb5
AnnaK-Cardoso <annakarolinecardoso123@gmail.com>
2023-03-16 22:08:54 -0300

    feat: Courses interface

---
+++ src/interfaces/Courses.ts
@@ -0,0 +1,9 @@
+export interface Courses {
+    name: string;
+    numberOfVideos: number;
+    avatar: string;
+    createdBy: string;
+    rating: number;
+    description: string;
+    userId: string;
+}
