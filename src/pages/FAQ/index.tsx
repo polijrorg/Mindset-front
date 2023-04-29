7e416d8f7e8a2390e0e2371913ef23b552bedd42
AnnaK-Cardoso <annakarolinecardoso123@gmail.com>
2023-04-29 00:31:49 -0300

    feat: FAQ page

---
+++ src/pages/FAQ/index.tsx
@@ -0,0 +1,14 @@
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+import * as S from './styles';
+
+const FAQ = () => {
+    return (
+        <>
+            <Header />
+            <S.Wrapper />
+            <Footer />
+        </>
+    );
+};
+export default FAQ;
---
+++ src/pages/mindset/faq.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+import FAQ from 'pages/FAQ';
+
+const Questions: NextPage = () => {
+    return <FAQ />;
+};
+export default Questions;
