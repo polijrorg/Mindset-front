e0f270efe4ac0e3b6b109a933083e120be1c3412
AnnaK-Cardoso <annakarolinecardoso123@gmail.com>
2023-06-06 20:27:08 -0300

    star

---
+++ src/components/Rating/styles.ts
@@ -0,0 +1,12 @@
+/* import styled from 'styled-components';
+
+interface SelectedRating {
+    value: number;
+}
+export const Star = styled.img`
+    width: 16px;
+    height: 16px;
+`;
+export const Container = styled.div`
+`;
+*/
